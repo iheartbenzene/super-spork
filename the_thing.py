70a7d3445d2f86f62bb9717ec06edc00e4c8f920
Jevon Morris <iheartbenzene@gmail.com>
2019-05-16 11:59:26 -0700

    initialize pygame

--- the_thing.py
+++ the_thing.py
@@ -1,37 +1,65 @@
-import numpy as np
-import scipy as sp
-import sklearn as sk 
-import random
+# import numpy as np
+# import scipy as sp
+# import sklearn as sk 
+# import random
 
 
-class Room(object):
-    # length = 
-    # width = 
-    # point = 
-    def  __init__(self, l, w, p):
-        self.length, self.width, self.point = l, w, p
+# class Room(object):
+#     # length = 
+#     # width = 
+#     # point = 
+#     def  __init__(self, l, w, p):
+#         self.length, self.width, self.point = l, w, p
 
-class Dungeon(object):
-    tile = Tile()
-    G = [[tile('X') for i in range(100)]for i in range(100)]
-    def makeDungeon(nRooms):
-      rooms = set(key, value, coded_value)
-      for n in range(nRooms):
-          r = room(randomDim(roomDim))
-          rooms.add(room())
+# class Dungeon(object):
+#     tile = Tile()
+#     G = [[tile('X') for i in range(100)]for i in range(100)]
+#     def makeDungeon(nRooms):
+#       rooms = set(key, value, coded_value)
+#       for n in range(nRooms):
+#           r = room(randomDim(roomDim))
+#           rooms.add(room())
 
-class Tile(object):
-    image 
-    def __init__(self, c):
-        self.image = c
+# class Tile(object):
+#     image 
+#     def __init__(self, c):
+#         self.image = c
 
-class Player(object):
-    pass
+# class Player(object):
+#     pass
 
-class Enemy(object):
-  pass
+# class Enemy(object):
+#   pass
 
-def randomDim(dimensions):
-    # pass in (x0, x1, y0, y1)
-    # initialize random integers for x0, x1, y0, y1
-    pass
+# def randomDim(dimensions):
+#     # pass in (x0, x1, y0, y1)
+#     # initialize random integers for x0, x1, y0, y1
+#     pass
+
+
+import pygame
+import sys
+import os
+
+'''
+objects
+'''
+
+
+
+'''
+setup
+'''
+
+worldx = 960
+worldy = 720
+fps = 40
+anims_cycle = 4
+
+
+
+'''
+main loop
+'''
+
+
